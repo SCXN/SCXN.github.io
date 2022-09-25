98ae124199174d65610f0bb192790541873f0fce
olmpc <100712452+SCXN@users.noreply.github.com>
2022-09-25 05:41:03 -0700

    Fix typos, pretty-up <head>, create /home/index
    ## contemporaryConsultant
    - Fix code typos on footers at /experience/index and /services/index
    ## CityULTRA
    - Clean-up linked styles and scripts on each page
    - Pretty <head> on each page
    - Create missing home/index.html

--- projects/CityULTRA/javascripts/bookings.js
+++
@@ -1,2 +0,0 @@
-!function(e,t,n,s,i,c){i=t.createElement(n),c=t.getElementsByTagName(n)[0],i.async=1,
-	i.src=s,c.parentNode.insertBefore(i,c)}(window,document,"script","https://www.picktime.com/assets/booking.js");
